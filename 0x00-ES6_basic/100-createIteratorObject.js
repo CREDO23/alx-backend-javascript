1ec9bc2008a75b56aedaf9621de53de3186ac203
CREDO23 <thierrybakera12@gmail.com>
2023-11-27 16:01:05 +0200

    fix: eslint for advanced tasks

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 export default function createIteratorObject(report) {
     const myarray = [];
     for (const item of Object.values(report.allEmployees)) {
--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,4 +1,4 @@
-/* eslint-disable radix */
+/* eslint-disable */
 export default function iterateThroughObject(reportWithIterator) {
     let output = '';
   
