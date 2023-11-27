8005e95dcdbc8d21a8a202817f46b06917b79ca2
CREDO23 <thierrybakera12@gmail.com>
2023-11-27 15:32:12 +0200

    feat: solution 9

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -4,9 +4,11 @@ export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
+
     getIncomeInDollars(income) {
       return `$${income}`;
     },
+
     getIncomeInEuros(income) {
       return `${income} euros`;
     },
