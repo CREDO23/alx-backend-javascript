c3bb139718a85f69f5ee4a1d047861a8069ec646
CREDO23 <thierrybakera12@gmail.com>
2023-11-28 14:49:23 +0200

    feat: add solution 6-7

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,19 +1,9 @@
 def pascal_triangle(n):
-    triangle = []
-    if (n <= 0):
-        triangle
-    else:
-        triangle.append([1])
-        i = 1
-        while (i <= n):
-            arr = [1]
-            k = 1
-            while (k < n):
-                arr.append(triangle[i-1][k-1] + triangle[i-1][k])
-                k += 1
-            triangle.append(arr)
-            i +=1
+    if (n <= 0 ):
+        return []
 
-        return triangle
+    triangle = [[1]]
 
+
+    return 
 print(pascal_triangle(3))
