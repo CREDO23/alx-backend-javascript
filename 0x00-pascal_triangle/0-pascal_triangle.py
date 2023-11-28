70979d2f69dfe2ba4731563cc233f38e1ce8ce14
CREDO23 <thierrybakera12@gmail.com>
2023-11-28 15:56:11 +0200

    feat: add solution (pascal triangle)

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,9 +1,18 @@
 def pascal_triangle(n):
-    if (n <= 0 ):
-        return []
+    triangle = []
 
-    triangle = [[1]]
+    if (n == 0):
+        return triangle
 
+    triangle.append([1])
 
-    return 
-print(pascal_triangle(3))
+    for i in range(1, n):
+        arr = [1]
+
+        for j in range(1, i):
+            arr.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+
+        arr.append(1)
+        triangle.append(arr)
+
+    return triangle
