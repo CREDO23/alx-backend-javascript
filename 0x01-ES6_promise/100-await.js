ba8cb93ad09a058dde37409060ce497309821919
CREDO23 <thierrybakera12@gmail.com>
2023-11-29 20:49:31 +0200

    refact: advanced solution

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,11 +1,11 @@
+/* eslint-disable */
 import { uploadPhoto, createUser } from './utils.js';
 
 async function asyncUploadUser() {
   const photo = await uploadPhoto();
   const user = await createUser();
 
-  return photo && user ? { photo, user } : { photo: null, user: null };
+  return { photo: photo ?? null, user: user ?? null };
 }
 
-
-export default asyncUploadUser
+export default asyncUploadUser;
