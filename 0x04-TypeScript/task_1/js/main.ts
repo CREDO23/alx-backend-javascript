009c77de0362a6a685dac9272e007e35d4e5f24d
CREDO23 <thierrybakera12@gmail.com>
2023-12-07 00:07:16 +0200

    feat: Add solution for task 1,2,3,4,5

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,35 +1,51 @@
-interface Teacher {
+export interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
   fullTimeEmployee: boolean;
-  yearsOfExperience: number;
+  yearsOfExperience?: number;
   location: string;
-  [key: string]: any;
+  [index:string]: any;
 }
 
-interface Directors extends Teacher {
+export interface Directors extends Teacher {
   numberOfReports: number;
 }
 
-type printTeacherFunction = (firstName: string, lastName: string) => string;
+export interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
 
-const printTeacher: printTeacherFunction = (firstName, lastName) => `${firstName[0]}. ${lastName}`;
+export function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName[0]}. ${lastName}`;
+}
 
-// Student class
-class StudentClass {
-  firstName: string;
-  lastName: string;
+export interface IStudentClassConstructor {
+  new (firstName: string, lastName: string): IStudentClass;
+}
+
+export interface IStudentClass {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+export class StudentClass implements IStudentClass {
+  private _firstName!: string;
+  private _lastName!: string;
 
   constructor(firstName: string, lastName: string) {
-    this.firstName = firstName;
-    this.lastName = lastName;
+    this._firstName = firstName;
+    this._lastName = lastName;
   }
 
-  workOnHomework() : string{
-      return 'Currently working'
+  workOnHomework() {
+    return 'Currently working';
   }
 
-  displayName() : string{
-    return this.firstName
+  displayName() {
+    return this._firstName;
   }
+}
+
+export function createStudent(ctor: IStudentClassConstructor, firstName: string, lastName: string): IStudentClass {
+  return new ctor(firstName, lastName);
 }
